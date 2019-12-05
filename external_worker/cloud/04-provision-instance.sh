433e07730dd208d48b40468b1f354392d3233f3d
Moshe Demri <mdemri@gmail.com>
2019-12-05 17:01:04 +0200

    working

--- external_worker/cloud/04-provision-instance.sh
+++ external_worker/cloud/04-provision-instance.sh
@@ -2,6 +2,7 @@ RESOURCE_GROUP_NAME=enigma-external-worker
 VM_NAME=enigma-external-worker
 VM_IMAGE=microsoft-azure-compute:azureconfidentialcompute:acc-ubuntu-16:latest
 VM_SIZE=Standard_DC2s
+
 az vm create -n $VM_NAME -g $RESOURCE_GROUP_NAME --image $VM_IMAGE   \
     --public-ip-address enigma-external-worker-ip --size $VM_SIZE  \
-    --admin-username enigma --ssh-key-value enigma.key.pub  # --custom-data cloud-init.txt
+    --admin-username enigma --ssh-key-value enigma.key.pub --custom-data cloud-init.txt
