30c6fa13a7c7c60f15ac70effdbbb8a7de4efb61
Reuven Podmazo <reuven@enigma.co>
2019-12-10 15:55:35 +0200

    salad client no longer fetches the Enimga SmC addresses

--- salad/client/scripts/run.sh
+++ salad/client/scripts/run.sh
@@ -3,7 +3,6 @@
 set -e
 
 export OPERATOR_HOST='salad_operator'
-export CONTRACT_ADDRESS_HOST='contract'
 
 sed -i -e 's/ETH_HOST=localhost/ETH_HOST=contract/g' .env
 sed -i -e 's/ENIGMA_HOST=localhost/ENIGMA_HOST=worker/g' .env
@@ -12,7 +11,6 @@ sed -i -e 's/MONGO_HOST=localhost/MONGO_HOST=mongo/g' .env
 sed -i -e 's/OPERATOR_HOST=localhost/OPERATOR_HOST=salad_operator/g' .env
 
 ./scripts/wait_for_network.sh
-./scripts/fetch_enigma_contract_addresses.sh
 
 echo 'You may start running tests.'
 
