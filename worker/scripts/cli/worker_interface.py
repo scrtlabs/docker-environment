988e0d890fbdc0a6ee62b762fb02e3bd07fd54a1
Dan <dbriggsie@gmail.com>
2019-12-19 20:52:48 -0500

    update failed command return

--- worker/scripts/cli/worker_interface.py
+++ worker/scripts/cli/worker_interface.py
@@ -26,7 +26,7 @@ class WorkerInterface:
                 if response.status == 200:
                     return 'Success!'
                 else:
-                    return 'Failed - See logs or something'
+                    return 'Failed - See detailed logs located in your worker-p2p ssh window for more information!'
 
     async def get_status(self):
         filename = f'{self.worker_config["ETH_KEY_PATH"]}{self.worker_config["STATUS_FILENAME"]}'
