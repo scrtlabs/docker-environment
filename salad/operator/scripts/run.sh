92d42257a5f050c9bbbf4c77e0a396db8a5131c5
Reuven Podmazo <reuven@enigma.co>
2020-01-22 18:13:04 +0200

    after deployment of the salad contracts, we now wait for one epoch to pass.
    This is a workaround to a current issue where if the deployment
    and the first task happen in the same epoch, we hit several race
    conditions in the protocol implementation

--- salad/operator/scripts/run.sh
+++ salad/operator/scripts/run.sh
@@ -8,6 +8,7 @@ if ! ./scripts/take_salad_contract_addresses_from_environment.js; then
     echo 'Running deployment...'
     ENV=$(echo "$ENIGMA_ENV" | tr '[:upper:]' '[:lower:]')
     npx truffle migrate --network $ENV
+    ./scripts/wait_for_epoch_transition.js
     echo 'Done deployment!'
 fi
 
