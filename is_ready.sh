d93b534da234ba06294bdc7d9677ab130a8369f7
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-01-29 22:01:30 -0700

    MAINT: making is_ready both Linux and Mac compatible

--- is_ready.sh
+++ is_ready.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-REGISTERED_WORKERS_IN_KM=$(
-    docker-compose logs --tail 1000 km |
-        grep -P 'get_active_workers|Confirmed epoch with worker params' |
-        tail -1 |
-        grep -Poi '0x[a-f0-9]+' |
-        sort -u |
-        wc -l
-)
+case "$(uname)" in
+	Darwin*)
+		REGISTERED_WORKERS_IN_KM=$(
+		    docker-compose logs --tail 1000 km |
+		        grep -E 'get_active_workers|Confirmed epoch with worker params' |
+		        tail -1 |
+		        grep -Eoi '0x[a-f0-9]+' |
+		        sort -u |
+		        wc -l
+		)
+		;;
+	*)
+		REGISTERED_WORKERS_IN_KM=$(
+		    docker-compose logs --tail 1000 km |
+		        grep -P 'get_active_workers|Confirmed epoch with worker params' |
+		        tail -1 |
+		        grep -Poi '0x[a-f0-9]+' |
+		        sort -u |
+		        wc -l
+		)
+		;;
+esac
 
 if [[ "$REGISTERED_WORKERS_IN_KM" -eq 0 ]]; then
     exit 1
